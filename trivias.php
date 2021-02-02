bd7bf05aa35f507b47acb8870fee5b85a6c0f023
davidildefonso <70923884+davidildefonso@users.noreply.github.com>
2021-02-02 09:21:30 -0500

    Update trivias.php

--- trivias.php
+++ trivias.php
@@ -7,7 +7,7 @@
   <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Comic+Neue&display=swap" rel="stylesheet">
   <?php include("connectDB.php") ;
-  $con=connectToDB("localhost","root","","trivias");
+$con=connectToDB("localhost","user","pass","trivias"); //change user and pass to your database user and password
 
   ?>
   <style>
